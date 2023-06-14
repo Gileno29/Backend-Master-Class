9d4300be27d4df59f1b3d556f2c2effe07cdb046
Gileno Duarte <gileno.cr.duarte@gmail.com>
2023-06-13 22:12:29 -0300

    add more querys for accounts tables

--- db/query/account.sql
+++ db/query/account.sql
@@ -7,3 +7,21 @@ INSERT INTO accounts (
   $1, $2, $3
 )
 RETURNING *;
+
+
+-- name: GetAccount :one
+SELECT * FROM accounts
+WHERE id = $1 LIMIT 1;
+
+-- name: ListAcconts :many
+SELECT * FROM accounts
+ORDER BY name;
+
+-- name: DeleteAccount :exec
+DELETE FROM accounts
+WHERE id = $1;
+
+-- name: UpdateAccount :exec
+UPDATE accounts
+  set balance = $2
+WHERE id = $1;
